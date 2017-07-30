0b8762e8ab1079d4548db5e0e4a59c1bf9f64284
yuhuan <597270222@qq.com>
2017-07-30 14:58:38 +0800

    add file

---
+++ addmore_jQuery/.router.js
@@ -0,0 +1,19 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/getNews',function(req,res){
+	var pageIndex = req.query.index
+	var len = 5
+	var data =[]
+
+	for(var i = 0;i<len;i++){
+		data.push('新闻'+(parseInt(pageIndex*len)+i+3))
+	}
+	
+
+	res.send({
+		status: 0,
+		data: data
+	})
+})}
+ module.exports.setRouter = setRouter
